4b57630b2e40f4e0a8661ff022e9ce84d30e81d8
AndrewNoh <32383693+AndrewNoh@users.noreply.github.com>
2023-09-06 15:25:37 +0900

    migration - version up spring boot 3.1.0

--- src/main/java/andrew/backend/app/BackendApplication.java
+++ src/main/java/andrew/backend/app/BackendApplication.java
@@ -1,8 +1,16 @@
 package andrew.backend.app;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
+@EnableJpaAuditing //JPA Auditing을 활성화
+@EnableScheduling //스프링에서 스케줄링을 사용
 @SpringBootApplication
 public class BackendApplication {
 
